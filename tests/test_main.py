a9f6e8ecd598e26f4019a77df954ca3cbdb617aa
Ander Diaz Gil-Negrete <adiazgil-ne@unav.es>
2022-02-09 11:42:30 +0100

    div function added

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,3 +29,10 @@ def test_subtract_two_numbers():
     result = calculator.sub(10, 4)
     
     assert result == 6
+    
+def test_divide_two_numbers():
+    calculator = Calc() # Instantation
+    
+    result = calculator.div(10, 4)
+    
+    assert result == 2.5
